77dbe03fb9334fb4f702d9612bfd95bdd61a3238
John <John>
2016-10-25 15:17:20 -0700

    finished exercise 2 and 3

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,12 +4,22 @@
 # This is a perfect situation in which writing a function will allow you to better structure your
 # code, and give a name to a repeated task.
 library(jsonlite)
-setwd('~/Documents/info-201/m10-apis/exercise-2/')
+
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
-
-
+TopTenSongs <- function(artist) {
+  base <- 'https://api.spotify.com/v1/'
+  type <- '&type=artist'
+  query_url <- paste0(base, 'search?', 'q=', artist, type)
+  data <- fromJSON(query_url)
+  artist.id <- data$artists$items$id[1]
+  
+  songs_url <- paste0(base, 'aristst/', artist.id, '/top-tracks?country=US')
+  songs <- fromJSON(songs_url)
+  return (songs$tracks)
+}
+Nelly <- TopTenSongs('Nelly')
 
 # What are the top 10 tracks by Nelly?
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -3,13 +3,22 @@ library(jsonlite)
 library(dplyr)
 # Use the `load` function to load in the nelly_tracks file.  That should make the data.frame
 # `top_nelly` available to you
-
+load('nelly_tracks.Rdata')
 
 # Use the `flatten` function to flatten the data.frame -- note what differs!
-
+new.nelly <- flatten(top.nelly)
 
 # Use your `dplyr` functions to get the number of the songs that appear on each albumt
+num.album <- new.nelly %>% 
+  group_by(album.name) %>% 
+  summarise(n = n()) %>% 
+  arrange(-n)
 
 
 
 # Bonus: perform both of the steps above in one line (one statement)
+num.album <- top.nelly %>% 
+  flatten %>% 
+  group_by(album.name) %>% 
+  summarise(n = n()) %>% 
+  arrange(-n)
